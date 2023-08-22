4506eba605c209c08227e4ebc14b181eb2b5a23b
José Carlos <69639102+josecarlossoares@users.noreply.github.com>
2023-08-22 12:47:19 +0000

    Respostas

---
+++ script.js
@@ -0,0 +1,45 @@
+//Primeiro
+// let lista = [3, 7, 2, 9, 5];
+// for(item of lista){
+//     console.log(item)
+// }
+
+//Segundo
+// let objeto = {a: 10, b: 20, c: 30, d: 40};
+// let soma = 0;
+// for(item in objeto){
+//     soma += objeto[item]
+// }
+// console.log(soma)
+
+//Terceiro
+// let lista = [12, 5, 8, 21, 16, 7]
+// let result = []
+
+// for (item of lista){
+//     if(item % 2 === 0){
+//         result.push(item)
+//     }
+// }
+
+// console.log(result)
+
+//Quarto
+// let  estudantes = [{nome:'Alice', idade:18}, {nome:'bob',idade:20}, {nome:'carol',idade:19}, {nome:'david',idade:21}]
+
+// for (i in estudantes){
+//     if(estudantes[i].idade == 19){
+
+//         console.log(`Nome:${estudantes[i].nome}. idade:${estudantes[i].idade}.`)
+//     }
+// }
+
+//Quinto
+// const pontos = [ [2, 5, 8], [3, 9, 12], [4, 6, 10] ];
+// let soma = 0;
+// for(let index in pontos){
+//     for(item in pontos[index]){
+//         soma += pontos[index][item]
+//     }
+// }
+// console.log(soma)
